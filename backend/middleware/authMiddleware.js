dcb99b54d38c044bf2e509f92eeeaf6fb460b0e5
Iyakem Estifanos <iyakem16@gmail.com>
2022-12-18 14:06:59 +0300

    admin api  and controllers

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -25,10 +25,17 @@ const protect = asyncHandler(async (req, res, next) => {
         res.status(401)
         throw new Error ('not authorized')
     }
-
-   
-
     
 })
 
-export {protect}
+
+const admin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+      next()
+    } else {
+      res.status(401)
+      throw new Error('Not authorized as an admin')
+    }
+  }
+
+export {protect, admin}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,15 @@
 import express from 'express'
 const router = express.Router()
-import {authUser, getUserProfile, registerUser, updateUserProfile, getTravelers, getConsumers} from '../controllers/userController.js'
+import {authUser, getUserProfile, registerUser, updateUserProfile, getTravelers, getConsumers, getUsers, getUserById, deleteUser, updateUser} from '../controllers/userController.js'
 import {protect} from '../middleware/authMiddleware.js'
 
 
-router.route ('/').post(registerUser)
+router.route ('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 router.route('/travelers').get(getTravelers)
 router.route('/consumers').get(getConsumers)
+router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser)
 
 
 
