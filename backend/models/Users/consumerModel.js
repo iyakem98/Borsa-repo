528b2c3119969ce22f5590803656656aeb84f48e
barkotefuye <defuye@hmc.edu>
2023-01-02 20:13:19 +0300

    updated consumer model

--- backend/models/Users/consumerModel.js
+++ backend/models/Users/consumerModel.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import destinationSchema from './sharedSchemas/destinationSchema';
 
 const itemSchema = mongoose.Schema({ 
-    //this is a separate schema for each item a buyer wants 
     itemType: {
         type: String,
         required: true
@@ -14,17 +13,11 @@ const itemSchema = mongoose.Schema({
     orderDate: {
         type: Date,
         default: Date.now
-    }
-});
-
-const itemsSchema = mongoose.Schema({
-    itemList: [itemSchema],  //this is supposed to handle a list of items that a buyer wants 
-    totalItemWeight: { //this handles the added total weight of all the items that a buyer wants
-        type: String,
-        required: true,
     },
+
     destination: destinationSchema
 });
+
 
 const consumerSchema = mongoose.Schema({ 
         user: {
@@ -32,7 +25,7 @@ const consumerSchema = mongoose.Schema({
             ref: "User",
             unique: true
         },
-        itemsList: [itemsSchema], // List of types of items user wants
+        itemsList: [itemSchema], // List of types of items user wants
         showBuyerCard: { //this is a boolean on whether or not a buyer wants to be visible to others 
             type: Boolean,
             required: true,
